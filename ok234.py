ebcad9fb1fae748dac59f7a70189879534bc4f4e
Lao Takanava <laoprog@gmail.com>
2024-12-05 12:19:35 +0300

    ok.py

--- ok234.py
+++ ok234.py
@@ -3,7 +3,7 @@ description = ('I think now nothing, but in future it will be OSINT programm.')
 installation = ('You can download and update it on https://github.com/lao-py/laosint.git')
 support = ('You can contact us and report bugs on https://t.me/laosint_bot.')
 creator = ('Lao Takanava. TGCH: https://t.me/x_lao_tg, https://t.me/laosint')
-status = ('alfa-testing, will created at 03.11.2024')
+status = ('alfa-testing, will created at **.**.20**')
 
 readme = '1. '+name+'\n'+'2. '+description+'\n'+'3. '+installation+'\n'+'4. '+support+'\n'+'5. '+creator+'\n'+'6. '+status
 print(readme)
